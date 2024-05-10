0fa344bb301b42649711eeb23ab64d323b3a7115
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-05-10 15:36:25 +0200

    ADD Class Pizza

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FotoUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public Pizza( string name, string description, string fotoUrl, decimal price)
+        {
+            Name = name;
+            Description = description;
+            FotoUrl = fotoUrl;
+            Price = price;
+        }
+    }
+}
